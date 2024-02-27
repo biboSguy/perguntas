4051357e0798debdc107817a7a47e0815b7cf981
biboSguy <132067957+biboSguy@users.noreply.github.com>
2024-02-27 07:58:35 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -22,8 +22,22 @@ def perguntas():
                 else:
                     print("digite valores validos")
                     continue
-                break
             except ValueError:
                 print("Por favor, digite valores válidos para a segunda pergunta.")
+            while True:
+                try:
+                    hetero =(input("Você é hetero?: "))
+                    if hetero.lower() == 'sim':
+                        print("hetero")
+                    elif hetero.lower() == 'nao':
+                        print("viadogay")
+                    else:
+                        print("digite valores validos")
+                        continue
+                    break
+                except ValueError:
+                    print("Por favor, digite valores válidos para a segunda pergunta.")
+    
+            break
         break
 perguntas()
