b5ccbfa36cb784673e5a6ca45efdbbf117e5f373
biboSguy <132067957+biboSguy@users.noreply.github.com>
2024-02-27 07:36:44 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,29 @@
+def perguntas():
+    print("Perguntas")
+    while True:
+        try:
+            gay = (input("Você é gay?: "))
+            if gay.lower() == 'sim':
+                print("gay")
+            elif gay.lower() == 'nao':
+                print("hetero")
+            else:
+                print("digite valores validos")
+                continue
+        except ValueError:
+            print("Por favor, digite números válidos.")
+        while True:            
+            try:
+                viado = (input("Você é viado?: "))
+                if viado.lower() == 'sim':
+                    print("viado")
+                elif viado.lower() == 'nao':
+                    print("hetero")
+                else:
+                    print("digite valores validos")
+                    continue
+                break
+            except ValueError:
+                print("Por favor, digite valores válidos para a segunda pergunta.")
+        break
+perguntas()
